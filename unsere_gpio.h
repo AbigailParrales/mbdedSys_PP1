512918842755f00a1352bbafb805068933c96a06
EnriqueLuna <54048229+EnriqueLuna@users.noreply.github.com>
2020-04-16 03:05:47 -0500

    Update unsere_gpio.h

--- unsere_gpio.h
+++ unsere_gpio.h
@@ -1,6 +1,30 @@
+
+/*********************************************************************
+* Copyright 2020 ITESM                                               *
+*                                                                    *
+*                                                                    *
+* ADC_UART                                                           *
+*                                                                    *
+* By                                                                 *
+* Jesús Enrique Luna Medina A01632334                                *
+* Daniela abigail Parrales Mejía A01228629                           *
+* Luis Cortés Leal A01631163                                         *
+*                                                                    *
+* Abril 2020                                                         *
+* The project titled ADC_UART is carried out with the purpose of     *
+* implementing a temperature sensing system which acquires the       *
+* values from the environment and delivers a response through        *
+* hardware using LEDs and software displaying on a Terminal.         *
+*                                                                    *
+*********************************************************************/
+
 #ifndef UNSERE_GPIO_H  
 #define UNSERE_GPIO_H 
 
+/********************************************************************/
+
 void gpio_setup(void);
+
+/********************************************************************/
 
 #endif
