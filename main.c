0e21300dbb893415f72ba2c823be2c53f56344fb
EnriqueLuna <54048229+EnriqueLuna@users.noreply.github.com>
2020-04-16 00:45:46 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,188 +1,48 @@
-/******************************************************************
-* Copyright 2020 ITESM                                            *
-*                                                                 *                                                                                     *
-* ADC_UART                                                        *
-*                                                                 *
-* By                                                              *
-* Jesús Enrique Luna Medina A01632334                             *
-* Daniela abigail Parrales Mejía A01228629                        *
-* Luis Cortés Leal A01631163                                      *
-*                                                                 *
-* Abril 2020                                                      *
-*                                                                 *
-* The project titled ADC_UART is carried out with the purpose of  *
-* implementing a temperature sensing system which acquires the    *
-* values from the environment and delivers a response through     *
-* hardware using LEDs and software displaying on a Terminal.      *
-*                                                                 *
-******************************************************************/
-
-///FUNCTIONS
-/**
-* BRIEF description ,parameters and return values
-*/
-
-///STRUCTS
-/**
-* structs, enums, constants, global variables
-*/
-
-///HIGH LEVEL 
-
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
-#include <libopencm3/stm32/adc.h>
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/usart.h>
-#include <libopencm3/stm32/timer.h>
 
-#include "unsere_gpio.h"
-#include "unsere_adc.h"
-#include "unsere_timer.h"
 #include "unsere_uart.h"
 
-/*****************************************************************/
+// Configuration for UART in order to write and read the serial data
+void uart_setup(void) {
+    rcc_periph_clock_enable(RCC_GPIOA);
+    gpio_set_mode(GPIOA,
+		GPIO_MODE_INPUT,
+		GPIO_CNF_INPUT_ANALOG,
+		GPIO0);
+    rcc_periph_clock_enable(RCC_USART1);
 
-char tmp[] = "Temp: xxxC\r\n";
-char rxmsg[4] = "x";
-int16_t max = 200, min = 100;
-int i = 0;
-int j = 0;
-int k = 0;
-int16_t aux_min_max = 0;
+    //  Enable UART TX (PA9)
+    // In order to use the pin A9 to transmit the information.
+    gpio_set_mode(GPIOA,
+                  GPIO_MODE_OUTPUT_50_MHZ,
+                  GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
+                  GPIO_USART1_TX);
 
-/*****************************************************************/
+    //  Enable UART RX (PA10)
+    // In order to use the pin A10 to read the information.
+    gpio_set_mode(GPIOA,
+                  GPIO_MODE_INPUT,
+                  GPIO_CNF_INPUT_PULL_UPDOWN,
+                  GPIO_USART1_RX);
 
-int main(void) {
-    rcc_clock_setup_in_hse_8mhz_out_72mhz(); ///  Use this for "blue pill"
-    gpio_setup();
-    ADC_setup();
-    uart_setup();
-    timer_setup();
+    usart_set_baudrate(USART1, 9600);  //  Baudrate configurated at 9600
+    usart_set_databits(USART1, 8);  //  Data will be 8 bit long 
+    usart_set_stopbits(USART1, USART_STOPBITS_1);
+    usart_set_mode(USART1, USART_MODE_TX_RX);
+    usart_set_parity(USART1, USART_PARITY_NONE);
+    usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
 
+    nvic_enable_irq(NVIC_USART1_IRQ);   //  Global enable of interruption
+    usart_enable_rx_interrupt(USART1);   //  Local enable of interruption
 
-    for (;;) {}
-
-    return 0;
+    usart_enable(USART1);
 }
 
-/*****************************************************************/
-
-/** Interruption from uart, the x and n keys are configurate 
-* in order to set the maximum temperature and the minimum.
-* Also with the restriction of entering letters instead of numbers 
-* and displaying a message to inform the user of that warning.
-*/
-
-void usart1_isr(void) {
-    USART1_SR = ~(1<<5);  //  borrar bandera
-    char flag_set_min_max = 0;
-
-/** When the 'n' is pressed, the configuration to set the miminum
-* temperature is displayed.
-*/
-
-    if (usart_recv(USART1) == 'n') {
-        timer_disable_counter(TIM2);
-        adc_power_off(ADC1);
-        uart_send("Set Min: ");
-        flag_set_min_max = 1;
-    }
-
-/** When the 'x' is pressed, the configuration to set the maximum
-* temperature is displayed.
-*/
-
-    if (usart_recv(USART1) == 'x') {
-        timer_disable_counter(TIM2);
-        adc_power_off(ADC1);
-        uart_send("Set Max: ");
-        flag_set_min_max = 2;
-    }
-
-/** The value written by the user is verified if there are only 
-* numbers, if there is a letter in that space, the terminal will 
-* display a warning message. If there are only numbers the characters
-* are sent.
-*/
-    if (i < 4) {
-        rxmsg[i] = usart_recv(USART1);
-        i++;
-        if (rxmsg[j] == '\r') {  //  User Input Enter
-                while (j < (i-1)) {
-                    if ((rxmsg[j] >= 48 && rxmsg[j] <= 57)) {
-                        //  Valid input (numbers)
-                        aux_min_max = aux_min_max*10 + (rxmsg[i]-48);
-                        j++;
-                    } else {
-                        uart_send("\r\nInvalid, input only integers\r\n");
-                        break;
-                }
-            }
-
-            if (flag_set_min_max == 2) {
-                    max = aux_min_max;
-            } else if (flag_set_min_max == 1) {
-                    min = aux_min_max;
-            }
-
-            i = 0;
-            adc_power_on(ADC1);
-            timer_enable_counter(TIM2);
-        }
-    } else {
-        uart_send("\r\nError\r\n");
-        i = 0;
-        adc_power_on(ADC1);
-        timer_enable_counter(TIM2);
-    }
-}
-
-/*****************************************************************/
-
-/** The interruption of the used timer. Inside we found the 
-* convertion from the reading of the adc and it is compared 
-* the minimum with that value and also the maximum with the 
-* read value. Depending the conditions will turn on or turn 
-* off the leds.
-*/
-
-void tim2_isr(void) {
-    timer_clear_flag(TIM2, TIM_SR_UIF);  ///  Turn off flag
-
-    uint16_t adc_read;
-    adc_read = read_adc(1) * 330 / 4095;
-
-/** Comparing the value received from the adc with the maximum
-* to turn on the led on pin A5.
-*/
-    if (adc_read > max) {
-        gpio_clear(GPIOA, GPIO5);
-        gpio_set(GPIOA, GPIO7);
-
-/** Comparing the value received from the adc with the minimum
-* to turn on the led on pin A7.
-*/
-    } else if (adc_read < min) {
-        gpio_clear(GPIOA, GPIO7);
-        gpio_set(GPIOA, GPIO5);
-    }
-
-/** In order to evaluate the temperature every .5 seconds the 
-* following structure is implemented.
-*/
-    if (k == 4) {  ///  Send Temp every 0.5 seg
-        k = 0;
-        char t = 2;
-        char index = 8;
-        do {
-            tmp[index--] = (adc_read%10) + 48;
-            adc_read/=10;
-        }while(t--);
-
-        /// Send Temp
-        uart_send(tmp);
-    } else {
-        k++;
+void uart_send(char * txt) {
+    while (*txt != '\0') {
+        usart_send_blocking(USART1, *txt++);
     }
 }
