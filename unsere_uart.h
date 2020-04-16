21b94861f56ccf56648ea37afec2b9f8631d1d1b
EnriqueLuna <54048229+EnriqueLuna@users.noreply.github.com>
2020-04-16 03:28:17 -0500

    Update unsere_uart.h

--- unsere_uart.h
+++ unsere_uart.h
@@ -1,7 +1,32 @@
+
+/*********************************************************************
+* Copyright 2020 ITESM                                               *
+*                                                                    *
+*                                                                    *
+* ADC_UART                                                           *
+*                                                                    *
+* By                                                                 *
+* Jesús Enrique Luna Medina A01632334                                *
+* Daniela abigail Parrales Mejía A01228629                           *
+* Luis Cortés Leal A01631163                                         *
+*                                                                    *
+* Abril 2020                                                         *
+* The project titled ADC_UART is carried out with the purpose of     *
+* implementing a temperature sensing system which acquires the       *
+* values from the environment and delivers a response through        *
+* hardware using LEDs and software displaying on a Terminal.         *
+*                                                                    *
+*********************************************************************/
+
 #ifndef UNSERE_UART_H
 #define UNSERE_UART_H
 
+
+/********************************************************************/
+
 void uart_setup(void);
 void uart_send(char * txt);
+
+/********************************************************************/
 
 #endif
