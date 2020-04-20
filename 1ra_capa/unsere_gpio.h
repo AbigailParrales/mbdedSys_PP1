9fdd9207f1728e6b73184fac2592dda36114f6a1
EnriqueLuna <54048229+EnriqueLuna@users.noreply.github.com>
2020-04-20 17:35:05 -0500

    Update unsere_gpio.h

--- 1ra_capa/unsere_gpio.h
+++ 1ra_capa/unsere_gpio.h
@@ -1,33 +1,33 @@
 
-/*********************************************************************
-* Copyright 2020 ITESM                                               *
-*                                                                    *
-*                                                                    *
-* ADC_UART                                                           *
-*                                                                    *
-* By                                                                 *
-* Jesús Enrique Luna Medina A01632334                                *
-* Daniela abigail Parrales Mejía A01228629                           *
-* Luis Cortés Leal A01631163                                         *
-*                                                                    *
-* Abril 2020                                                         *
-* The project titled ADC_UART is carried out with the purpose of     *
-* implementing a temperature sensing system which acquires the       *
-* values from the environment and delivers a response through        *
-* hardware using LEDs and software displaying on a Terminal.         *
-*                                                                    *
-*********************************************************************/
+/************************************************************************
+* Copyright 2020 ITESM                                                  *
+*                                                                       *
+*                                                                       *
+* ADC_UART                                                              *
+*                                                                       *
+* Autorhs:                                                              *
+* Jesús Enrique Luna Medina          A01632334                          *
+* Daniela abigail Parrales Mejía     A01228629                          *
+* Luis Cortés Leal                   A01631163                          *
+*                                                                       *
+* Abril 2020                                                            *
+* The project titled ADC_UART is carried out with the purpose of        *
+* implementing a temperature sensing system which acquires the          *
+* values from the environment and delivers a response through           *
+* hardware using LEDs and software displaying on a Terminal.            *
+*                                                                       *
+************************************************************************/
 
 #ifndef UNSERE_GPIO_H  
 #define UNSERE_GPIO_H 
 
-/********************************************************************/
+/***********************************************************************/
 
 void gpio_setups_for_adc(void);
 void gpio_setups_for_uart(void);
 void gpio_setups_for_LEDs(void);
 void gpio_setup(void);
 
-/********************************************************************/
+/***********************************************************************/
 
 #endif
