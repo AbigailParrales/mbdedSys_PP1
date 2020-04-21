6a0e051eb56181cc34476926218724c941ec6548
EnriqueLuna <54048229+EnriqueLuna@users.noreply.github.com>
2020-04-20 22:35:02 -0500

    Update unsere_log.h

--- 2da_capa/unsere_log.h
+++ 2da_capa/unsere_log.h
@@ -1,5 +1,28 @@
+  
+/************************************************************************
+* Copyright 2020 ITESM                                                  *
+*                                                                       *
+*                                                                       *
+* ADC_UART                                                              *
+*                                                                       *
+* Autorhs:                                                              *
+* Jesús Enrique Luna Medina          A01632334                          *
+* Daniela abigail Parrales Mejía     A01228629                          *
+* Luis Cortés Leal                   A01631163                          *
+*                                                                       *
+* Abril 2020                                                            *
+* The project titled ADC_UART is carried out with the purpose of        *
+* implementing a temperature sensing system which acquires the          *
+* values from the environment and delivers a response through           *
+* hardware using LEDs and software displaying on a Terminal.            *
+*                                                                       *
+************************************************************************/
+
+/*!< LIBRARIES */
 #ifndef UNSERE_LOG_H
 #define UNSERE_LOG_H
+
+/************************************************************************/
 
 void log_temperature(int temp, int max_umbral, int min_unbral, char* LED_low, char* LED_high);
 void log_message(char* message);
@@ -8,4 +31,8 @@ void log_ASCII(char ascii);
 void log_enter(void);
 void log_char(char ch);
 
+/************************************************************************/
+
 #endif
+
+/************************************************************************/
