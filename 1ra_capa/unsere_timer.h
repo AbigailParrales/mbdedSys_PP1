5f53f7bf7071c3e9043d8cff0ac44a89be0eeaa3
EnriqueLuna <54048229+EnriqueLuna@users.noreply.github.com>
2020-04-20 22:14:37 -0500

    Update unsere_timer.h

--- 1ra_capa/unsere_timer.h
+++ 1ra_capa/unsere_timer.h
@@ -1,23 +1,24 @@
 
-/*********************************************************************
-* Copyright 2020 ITESM                                               *
-*                                                                    *
-*                                                                    *
-* ADC_UART                                                           *
-*                                                                    *
-* By                                                                 *
-* Jesús Enrique Luna Medina A01632334                                *
-* Daniela abigail Parrales Mejía A01228629                           *
-* Luis Cortés Leal A01631163                                         *
-*                                                                    *
-* Abril 2020                                                         *
-* The project titled ADC_UART is carried out with the purpose of     *
-* implementing a temperature sensing system which acquires the       *
-* values from the environment and delivers a response through        *
-* hardware using LEDs and software displaying on a Terminal.         *
-*                                                                    *
-*********************************************************************/
+/************************************************************************
+* Copyright 2020 ITESM                                                  *
+*                                                                       *
+*                                                                       *
+* ADC_UART                                                              *
+*                                                                       *
+* Autorhs:                                                              *
+* Jesús Enrique Luna Medina          A01632334                          *
+* Daniela abigail Parrales Mejía     A01228629                          *
+* Luis Cortés Leal                   A01631163                          *
+*                                                                       *
+* Abril 2020                                                            *
+* The project titled ADC_UART is carried out with the purpose of        *
+* implementing a temperature sensing system which acquires the          *
+* values from the environment and delivers a response through           *
+* hardware using LEDs and software displaying on a Terminal.            *
+*                                                                       *
+************************************************************************/
 
+/*!< LIBRARIES */
 #ifndef UNSERE_TIMER_H
 #define UNSERE_TIMER_H
 
@@ -28,3 +29,5 @@ void timer_setup(void);
 /********************************************************************/
 
 #endif
+
+/********************************************************************/
