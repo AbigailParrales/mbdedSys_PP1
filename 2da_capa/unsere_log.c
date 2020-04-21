53393f4d9b595c55c16f346af204d63f18474ead
EnriqueLuna <54048229+EnriqueLuna@users.noreply.github.com>
2020-04-20 22:33:49 -0500

    Update unsere_log.c

--- 2da_capa/unsere_log.c
+++ 2da_capa/unsere_log.c
@@ -1,16 +1,41 @@
-#include <libopencm3/stm32/usart.h>
 
+/************************************************************************
+* Copyright 2020 ITESM                                                  *
+*                                                                       *
+*                                                                       *
+* ADC_UART                                                              *
+*                                                                       *
+* Autorhs:                                                              *
+* Jesús Enrique Luna Medina          A01632334                          *
+* Daniela abigail Parrales Mejía     A01228629                          *
+* Luis Cortés Leal                   A01631163                          *
+*                                                                       *
+* Abril 2020                                                            *
+* The project titled ADC_UART is carried out with the purpose of        *
+* implementing a temperature sensing system which acquires the          *
+* values from the environment and delivers a response through           *
+* hardware using LEDs and software displaying on a Terminal.            *
+*                                                                       *
+************************************************************************/
+
+/*!< LIBRARIES */
+#include <libopencm3/stm32/usart.h>
 #include "unsere_log.h"
 #include "../1ra_capa/unsere_uart.h"
 
+/***********************************************************************/
+
+/*!< Log the char received */
 void log_char(char ch) {
     usart_send_blocking(USART1, ch);
 }
 
+/*!< Log the ASCII character of the char received */
 void log_ASCII(char ascii) {
     log_char(ascii + 48);
 }
 
+/*!< Log the ASCII form of a three digit number */
 void log_3digs_number(int number) {
     char centenas = 0;
     char decenas = 0;
@@ -27,16 +52,19 @@ void log_3digs_number(int number) {
     log_ASCII(unidades);
 }
 
+/*!< Logs a message */
 void log_message(char* message) {
     while (*message != '\0') {
         usart_send_blocking(USART1, *message++);
     }
 }
 
+/*!< Logs the line jump and return to begining of the line */
 void log_enter(void) {
     log_message("\r\n");
 }
 
+/*!< Display on terminal temperature message */
 void log_temperature(int temp, int max_umbral, int min_unbral, char* LED_low, char* LED_high) {
     log_message("\r\nTemp: ");
     log_3digs_number(temp);
@@ -56,3 +84,4 @@ void log_temperature(int temp, int max_umbral, int min_unbral, char* LED_low, ch
     log_message("\r\n");
 }
 
+/***********************************************************************/
